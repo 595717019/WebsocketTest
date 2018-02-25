b8d12dc7d2f9e6a7e36e0860b6a4c83de5b3a73c
595717019 <595717019@qq.com>
2018-02-25 17:19:21 +0800

    初版

---
+++ src/com/websocket/ChatRoom.java
@@ -0,0 +1,136 @@
+package com.websocket;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Date;
+import java.util.List;
+
+import org.apache.log4j.Logger;
+import org.springframework.web.socket.BinaryMessage;
+import org.springframework.web.socket.CloseStatus;
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketSession;
+import org.springframework.web.socket.handler.AbstractWebSocketHandler;
+
+public class ChatRoom extends AbstractWebSocketHandler {
+    public static final String FILEPATHIMG = "uploadFiles/uploadImgs/"; // 图片上传路径
+    String filePath = getClasspath() + FILEPATHIMG; // 文件上传路径
+
+    public final static List<WebSocketSession> sessionList = Collections.synchronizedList(new ArrayList<WebSocketSession>());
+    SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
+    Logger logger = Logger.getLogger(this.getClass());
+    FileOutputStream output;
+
+    /*
+     * 获取classpath
+     */
+    public static String getClasspath() {
+        String path = (String.valueOf(Thread.currentThread().getContextClassLoader().getResource("")) + "../../").replaceAll("file:/", "").replaceAll("%20", " ").trim();
+        if (path.indexOf(":") != 1) {
+            path = File.separator + path;
+        }
+        return path;
+    }
+
+    @Override
+    public void afterConnectionEstablished(WebSocketSession webSocketSession) throws Exception {
+        System.out.println("Connection established..." + webSocketSession.getRemoteAddress());
+        System.out.println(webSocketSession.getAttributes().get("user") + " Login");
+        webSocketSession.sendMessage(new TextMessage((webSocketSession.getAttributes().get("user") + " :")));
+        sessionList.add(webSocketSession);
+    }
+
+    @Override
+    public void afterConnectionClosed(WebSocketSession webSocketSession, CloseStatus status) throws Exception {
+        System.out.println("Connection closed..." + webSocketSession.getRemoteAddress() + " " + status);
+        System.out.println(webSocketSession.getAttributes().get("user") + " Logout");
+        sessionList.remove(webSocketSession);
+    }
+
+    @Override
+    public void handleTextMessage(WebSocketSession websocketsession, TextMessage message) {
+        String payload = message.getPayload();
+        String textString;
+        try {
+            if (payload.endsWith(":fileStart")) {
+                output = new FileOutputStream(new File(filePath + payload.split(":")[0]));
+            } else if (payload.endsWith(":fileFinishSingle")) {
+                output.close();
+                String fileName = payload.split(":")[0];
+                for (WebSocketSession session : sessionList) {
+                    if (session.getId().equals(websocketsession.getId())) {
+                        textString = "我:" + format.format(new Date()) + "<br>";
+                    } else {
+                        textString = websocketsession.getAttributes().get("user") + ":" + format.format(new Date()) + "<br>";
+                    }
+                    TextMessage textMessage = new TextMessage(textString);
+                    session.sendMessage(textMessage);
+                    sendPicture(session, fileName);
+                }
+            } else if (payload.endsWith(":fileFinishWithText")) {
+                output.close();
+                String fileName = payload.split(":")[0];
+                for (WebSocketSession session : sessionList) {
+                    sendPicture(session, fileName);
+                }
+            } else {
+                for (WebSocketSession session : sessionList) {
+                    if (session.getId().equals(websocketsession.getId())) {
+                        textString = "我:" + format.format(new Date()) + "<br>" + payload;
+                    } else {
+                        textString = websocketsession.getAttributes().get("user") + ":" + format.format(new Date()) + "<br>" + payload;
+                    }
+                    TextMessage textMessage = new TextMessage(textString);
+                    session.sendMessage(textMessage);
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void handleBinaryMessage(WebSocketSession session, BinaryMessage message) {
+        ByteBuffer buffer = message.getPayload();
+        try {
+            output.write(buffer.array());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void handleTransportError(WebSocketSession webSocketSession, Throwable throwable) throws Exception {
+        if (webSocketSession.isOpen()) {
+            webSocketSession.close();
+        }
+        System.out.println(throwable.toString());
+        System.out.println("WS connection error,close..." + webSocketSession.getRemoteAddress());
+    }
+
+    @Override
+    public boolean supportsPartialMessages() {
+        return true;
+    }
+
+    public void sendPicture(WebSocketSession session, String fileName) {
+        FileInputStream input;
+        try {
+            File file = new File(filePath + fileName);
+            input = new FileInputStream(file);
+            byte bytes[] = new byte[(int) file.length()];
+            input.read(bytes);
+            BinaryMessage byteMessage = new BinaryMessage(bytes);
+            session.sendMessage(byteMessage);
+            input.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
